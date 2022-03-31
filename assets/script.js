ea19ef4c181ae1eb7e3b86a36275ff1347b6f99b
Trey S <fjschmidt292@gmail.com>
2022-03-30 22:19:51 -0400

    pre-merge commit

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+/*when generateImage is run:
+the calls to api will happen and we will receive a URL for an image, which will display in the center.
+we will also pull the information entered into the text field area and save it in local storage and use it to create a link to the URL that was pulled with that search
+by innerHTML-ing it to the the UL for past searches. 
+*/
+function generateImage(){
+    getElementById
+  }; 
+  
+  document.getElementById("btn ").addEventListener("click", generateImage)
